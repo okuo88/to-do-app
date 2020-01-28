8e6f6de5aa286611114cad4c1af460b7bf420583
OLIVIA KUO <o@OLIVIAs-MacBook-Pro.local>
2020-01-28 08:40:20 -0800

    Assignment 23: added new docs

---
+++ app.js
@@ -0,0 +1,34 @@
+function onReady() {
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+
+  addToDoForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let title = newToDoText.value;
+
+    let newLi = document.createElement('li');
+    let checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+
+    let deleteBtn = document.createElement('button')
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener('click', function(event){
+      //console.log(event);
+      //this.parentElement
+      toDoList.removeChild(this.parentElement);
+    })
+
+    newLi.textContent = title;
+    newLi.appendChild(checkbox);
+    newLi.appendChild(deleteBtn);
+    toDoList.appendChild(newLi);
+    newToDoText.value = '';
+  });
+};
+
+window.onload = function() {
+  onReady();
+ };
